049b4b214abe152d2468b27f8c571f426546be6b
patrickcaringal <patrickcaringalreal@gmail.com>
2022-11-01 18:28:52 +0800

    chore: fix build issue

--- pages/admin/appointments/approved.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-import AppointmentManagementPage from "../../../components/pages/admin/AppointmentList/list";
-
-const AppointmentApproval = () => <AppointmentManagementPage />;
-export default AppointmentApproval;
